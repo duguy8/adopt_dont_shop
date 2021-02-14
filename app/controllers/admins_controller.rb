c4ad08262d64f746f27af460de0ab3d1b907295c
Kris Litman <duguy8@gmail.com>
2021-02-14 14:11:18 -0500

    Application can be approved when all pets are approved, need to add model test for instance method approved

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -7,10 +7,13 @@ class AdminsController < ApplicationController
     if params[:approve]
       @application = Application.find(params[:id])
       @pet = @application.pets.find(params[:approve])
+      @pet.update({status: true, adoptable: false})
+      @application.approved?
       redirect_to "/admins/applications/#{@application.id}?approve=yes&pet_id=#{@pet.id}"
     elsif params[:reject]
       @application = Application.find(params[:id])
       @pet = @application.pets.find(params[:reject])
+      @pet.update({adoptable: true})
       redirect_to "/admins/applications/#{@application.id}?approve=no&pet_id=#{@pet.id}"
     end
   end
